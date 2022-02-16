7726322def9ca6c8e99ce6d5936efbb034c3a7e7
Shahid Rahim <shahid@cobold.in>
2022-02-17 02:13:16 +0530

    apply dark theme mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import 'remixicon/fonts/remixicon.css'
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
 import Head from 'next/head'
-import Image from 'next/image'
+import { FaRegMoon, FaSun } from 'react-icons/fa'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
+  const [isDark, setIsDark] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    if (isDark) {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }, [isDark, setTheme])
+
+  const hndleDarkMode = () => {
+    setIsDark(!isDark)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,14 +29,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Shahid Rahim
-        </h1>
+        <button onClick={hndleDarkMode} className={styles.darkMode}>
+          {isDark ? (
+            <span className={styles.darkMode_sun}>
+              <FaSun />
+            </span>
+          ) : (
+            <span className={styles.darkMode_moon}>
+              <FaRegMoon />
+            </span>
+          )}{' '}
+        </button>
+        <h1 className={styles.title}>Shahid Rahim</h1>
         <h2>Front end engineer</h2>
 
         <p className={styles.description}>
-          Check out.{' '}
-          <code className={styles.code}>what I can do</code>
+          Check out. <code className={styles.code}>what I can do</code>
         </p>
 
         <div className={styles.grid}>
@@ -33,18 +58,12 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href=""
-            className={styles.card}
-          >
+          <a href="" className={styles.card}>
             <h2>Wordpress Blogs Development &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href=""
-            className={styles.card}
-          >
+          <a href="" className={styles.card}>
             <h2>SEO Services &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -54,15 +73,8 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          Powered by <span className={styles.logo}>shahid-rahim</span>
         </a>
       </footer>
     </div>
